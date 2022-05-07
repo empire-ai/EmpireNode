8ea0ea21a7a122c6d77f11065f842eb103387689
Kaspar Kallas <kaspar@digitalsputnik.com>
2022-05-07 18:39:12 +0300

    Update new_main.py

--- empire/new_main.py
+++ empire/new_main.py
@@ -29,15 +29,24 @@ machine.Pin(27, machine.Pin.OUT, value=1)
 white = machine.PWM(machine.Pin(4),19000)
 
 def _interpolate_led_channel(c_id):
-    global target_rgbw, _vector_rgbw, _current_rgbw
     '''
     Helper function to *hw_update*, to split the logic per channel
     '''
-    if abs(_current_rgbw[c_id]-target_rgbw[c_id]) > 1:
+    
+    global target_rgbw, _vector_rgbw, _current_rgbw
+    
+    # if channel is within 1 codevalue target value is used
+    if abs(_current_rgbw[c_id]-target_rgbw[c_id]) > abs(_vector_rgbw[c_id]):
         ch_out = _current_rgbw[c_id]+_vector_rgbw[c_id]
     else:
         ch_out = target_rgbw[c_id]
     
+    # clip
+    if ch_out > 1023:
+        ch_out = 1023
+    if ch_out < 0:
+        ch_out =0
+        
     return ch_out
     
 
@@ -46,7 +55,7 @@ def hw_update(caller):
     Timer IRQ event to update all HW raleated IOs for properties
     '''
     global target_rgbw, _vector_rgbw, _current_rgbw, last_render
-    _t_start = time.ticks_ms()
+    #_t_start = time.ticks_ms()
     out = []
     for i in range(4):
         out.append(_interpolate_led_channel(i))
@@ -59,17 +68,25 @@ def hw_update(caller):
         
     _current_rgbw = tuple(out)
     time.sleep_ms(2)
-    _t_end = time.ticks_ms()
-    last_renderder = time.ticks_diff(_t_start,_t_end)
-    
+    #_t_end = time.ticks_ms()
+    # 0 last_renderder = time.ticks_diff(_t_start,_t_end)
+     
 def target_set(color_in):
-    global target_rgbw, _vector_rgbw, _current_rgbw, steps
+    global target_rgbw, _vector_rgbw, _current_rgbw, steps, last_render
+    _t_start = time.ticks_ms()
     target_rgbw = color_in
     out = [] 
     for i in range(4):
-        out.append(int((color_in[i]-_current_rgbw[i])/steps))
+        val = int((color_in[i]-_current_rgbw[i])/steps)
+        # if val is below 1 int cast will remove it so i just hardcoded it to 1
+        if val == 0:
+            val = color_in[i]-_current_rgbw[i]-abs(color_in[i]-_current_rgbw[i])
+            
+        out.append(val)
         
     _vector_rgbw = tuple(out)
+    _t_end = time.ticks_ms()
+    last_render = time.ticks_diff(_t_start,_t_end)
 
 #step size
 #min step size
